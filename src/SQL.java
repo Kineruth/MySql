7196e72ca973eec0b4d8c050b6c2b985dfc0a17a
Kineret <33723322+Kineruth@users.noreply.github.com>
2018-04-16 15:14:41 +0300

    Added Eclipse java files

---
+++ src/SQL.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+
+public class SQL {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static Connection getConnection() throws Exception{
+		String driver  = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/database";
+		return null;
+		
+		
+	}
+
+}
