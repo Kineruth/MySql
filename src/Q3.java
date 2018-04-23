d78424b209dab9cdaef716f330faf7e3d0f89962
Kineruth <naharykin@gmail.com>
2018-04-23 13:48:53 +0300

    Created a solution for Q3

---
+++ src/Q3.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		topTenPatients();
+	}
+
+	public static void topTenPatients() {
+		try {
+			Connection myConn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/ck?autoReconnect=true&useSSL=false", "root", "MyPass");
+			Statement myStmt = myConn.createStatement();
+			int myRs = myStmt.executeUpdate("CREATE VIEW top_ten_patients AS\r\n"
+					+ "SELECT patient_name FROM patients, queue, queue_reserved \r\n"
+					+ "ORDER BY ( time - queue_reserved.queue_time  )  limit 10; ");
+		
+//			while (myRs.next()) {
+//				System.out.println(myRs.getString("patient_name"));
+//			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
